e2ae95f4efa67e8ea6da18e4686c694d9e9c5e37
oleghenkel <oleg.henkel75@gmail.com>
2023-04-25 20:07:42 +0200

    Home Work Book

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-
-
 public class Main {
     public static void main(String[] args) {
+
+        Author king = new Author("Stephen ","King");
+        Author schildt = new Author("Herbert ", "Schildt");
+        Book fairyTale = new Book("Fairy Tale ", king, 2022);
+        Book javaProgramming = new Book("JAVA Programming ", schildt, 2007);
+        javaProgramming.setPublicationsYear(2014);
+        System.out.println(javaProgramming.getTitle() + schildt.getName() + schildt.getLastName() + " "
+                +javaProgramming.getPublicationsYear());
+        System.out.println(fairyTale.getTitle() + king.getName() + king.getLastName() + " " + fairyTale.getPublicationsYear());
+
+
 
     }
 }
